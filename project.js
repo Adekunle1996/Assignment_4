e6d812b98089c5b765c412532c6d215a476db90c
Adekunle1996 <olaniyandadekunle255@gmail.com>
2022-07-10 04:40:47 +0100

    project file

---
+++ project.js
@@ -0,0 +1,11 @@
+let totalAcc= 0;
+function sumOfNum(){
+    for(var x=0; x < arguments.length; x++){
+        if(!isNaN(arguments[x])){
+            totalAcc += arguments[x];
+        }
+    } 
+}
+sumOfNum(1, 2, 4, 6, 7, "boy", "orange", "bola", 10, 30, 40 , 60, 10, 50, "Emmanuel");
+console.log(totalAcc);
+   
